1d2e72c81b4a18156edc9fee6175e4fa90718750
Brian <brianccain@gmail.com>
2013-08-30 09:27:28 -0500

    Init Commit
    Adding new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MtdDashboard::Application.config.secret_token = '0cff05d2a6dc54b41da18710c58bc153623c164305daedda3d3ab49b19c5c1b7fb3e4b050ab5fd055cf354a363ebcc7d57d2f109da86252c2580555b9f8d6af8'
