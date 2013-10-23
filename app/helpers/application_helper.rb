a32e9d4c86838eac8d394ead264bcdf85398a021
Brian <brianccain@gmail.com>
2013-10-23 15:52:54 -0500

    Improve how dropdown highlight is determined
    This commit adds an application helper file that takes a simple string
    relating to the dropdown group that the layout file is checking. If that
    dropdown matches the current path, then it'll return true, making that
    dropdown highlighted on the navbar.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,18 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  # Tests to see if specified dropdown
+  # group should be highlighted or not
+  def highlight?(dropdown_group)
+    if dropdown_group == 'home'
+      ["/", "/dashboard/network"].any? do | route |
+        current_page?(route)
+      end
+    elsif dropdown_group == 'manage'
+      ["/dashboard/config", "/dashboard/index"].any? do | route|
+        current_page(route)
+      end
+    end
+  end
 end
