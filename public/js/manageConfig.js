c25ecc39f42ea316b26ea721cf59b3f5b8dec21b
Brian <brianccain@gmail.com>
2013-11-06 14:58:24 -0600

    Add input validation and config process
    This commit adds a couple features for the manage page:
    
    First it has a simple and extendable validation for user input. Simply,
    right now this just makes sure the user doesn't enter in something blank
    or too long. In the future, more requirements might be needed within the
    function.
    
    Second, this adds a more complex process of defining the goals config.
    It walks the user through what to configure, and when they have finished
    defining everything, it asks if the user is ready to continue
    configuration based on what they had defined.

--- public/js/manageConfig.js
+++ public/js/manageConfig.js
@@ -11,15 +11,7 @@ function loadSubGoalName() {
   var goalName = $('#goalNameText').val(),
       goalSubNameHTML = "";
 
-  if (goalName == "") {
-    alert("WARNING: You did not enter anything");
-    return;
-  }
-  else if (goalName.length >= 100) {
-    alert("WARNING: Your goal name is too long");
-    return;
-  }
-  else {
+  if (validateInput(goalName)) {
     goalSubNameHTML = "<br/><p>Give your goal a subname</p>";
     goalSubNameHTML += "<div class=\"input-group\"><input type=\"text\" class=\"form-control\" id=\"goalSubNameText\" placeholder=\"Enter sub-goal name\"><span class=\"input-group-btn\"><button class=\"btn btn-default\" type=\"button\" onclick=\"loadRoles()\">Go!</button></span></div><!-- /input-group -->";
 
@@ -31,18 +23,60 @@ function loadRoles() {
   var subGoalName = $('#goalSubNameText').val(),
       rolesHTML = "";
 
-  if (subGoalName == "") {
-    alert("WARNING: You did not enter anything");
-    return;
-  }
-  else if (subGoalName.length >= 500) {
-    alert("WARNING: Your sub-goal name is too long");
-    return;
-  }
-  else {
-    rolesHTML = "<br/><p>Add roles</p>";
+  if (validateInput(subGoalName)) {
+    rolesHTML = "<br/><p>Add comma separated roles (i.e. role1,role2,role3)</p>";
     rolesHTML += "<div class=\"input-group\"><input type=\"text\" class=\"form-control\" id=\"rolesText\" placeholder=\"Enter a role\"><span class=\"input-group-btn\"><button class=\"btn btn-default\" type=\"button\" onclick=\"addRole()\">Add</button></span></div><!-- /input-group -->";
 
     $('#roleList').html(rolesHTML);
+  }
+}
+
+function addRole() {
+  var roleListHTML = "<br/><p>Role List</p>",
+      newRoles = $('#rolesText').val(),
+      roleArr;
+
+  if (validateInput(newRoles)) {
+    // split on ,
+    roleArr = newRoles.split(",");
+
+    roleListHTML += "<div id=\"nodes\" class=\"tab-pane\"><select class=\"form-control\">";
+
+    for (var role in roleArr) {
+      console.log(roleArr[role]);
+      roleListHTML += "<option>" + roleArr[role] + "</option>";
+    }
+
+    roleListHTML += "</select></div>";
+
+    roleListHTML += "<br/><button type=\"button\" class=\"btn btn-primary\" onclick=\"loadGoalConfig()\">Continue</button>"
+    // loop through resulting array and add to dropdown, move to next state
+
+    $('#dropdownDiv').html(roleListHTML);
+  }
+}
+
+function loadGoalConfig() {
+
+}
+
+/*
+ *  Validates the given input from the config
+ *  text boxes.
+ *
+ *  Returns false if fails requirements, 
+ *  else returns true if input is "valid".
+ */
+function validateInput(input) {
+  if (input == "" ) {
+    alert("WARNING: You did not enter anything");
+    return false;
+  }
+  else if (input.length >= 500) {
+    alert("WARNING: Your entry name is too long");
+    return false;
+  }
+  else {
+    return true;
   }
 }
