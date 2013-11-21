24abd608b93866bacd9197f8dc317eaadb70dad3
Brian <brianccain@gmail.com>
2013-11-21 15:22:38 -0600

    Add node graph with visjs

--- public/js/dashboard/instances/networkGraph.js
+++ public/js/dashboard/instances/networkGraph.js
@@ -8,18 +8,29 @@
  */
 
 function drawNetwork(){
-  // jsPlumb.ready(function() {
-  // });
-
-  var container = document.getElementById('directedNetworkContainer');
-  var data = [
-      {id: 1, content: 'item 1', start: '2013-04-20'},
-      {id: 2, content: 'item 2', start: '2013-04-14'},
-      {id: 3, content: 'item 3', start: '2013-04-18'},
-      {id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19'},
-      {id: 5, content: 'item 5', start: '2013-04-25'},
-      {id: 6, content: 'item 6', start: '2013-04-27'}
+  // create an array with nodes
+  var nodes = [
+      {id: 1, label: 'Node 1'},
+      {id: 2, label: 'Node 2'},
+      {id: 3, label: 'Node 3'},
+      {id: 4, label: 'Node 4'},
+      {id: 5, label: 'Node 5'}
   ];
+
+  // create an array with edges
+  var edges = [
+      {from: 1, to: 2},
+      {from: 1, to: 3},
+      {from: 2, to: 4},
+      {from: 2, to: 5}
+  ];
+
+  // create a graph
+  var container = document.getElementById('directedNetworkContainer');
+  var data = {
+      nodes: nodes,
+      edges: edges
+  };
   var options = {};
-  var timeline = new vis.Timeline(container, data, options);
+  var graph = new vis.Graph(container, data, options);
 }
